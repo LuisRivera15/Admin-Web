cb616f910889dad261207fd597faf0ec1e169a9f
alanlopez0208 <alan666_alan@hotmail.com>
2023-06-21 12:15:36 -0600

    Añadiendo algo que no era jsjs

--- inventario.php
+++ inventario.php
@@ -28,9 +28,9 @@
         rel="stylesheet" />
 </head>
 <style>
-a {
-    text-decoration: none;
-}
+    a {
+        text-decoration: none;
+    }
 </style>
 
 <body>
@@ -252,7 +252,7 @@ a {
                 <div class="col-12">
                     <h2 class="text-center w-100">PRODUCTOS DE VENTA</h2>
                 </div>
-                <button id="botonAñadir" type="button" class="btn btn-warning" onclick="abiriAgregarModal()">
+                <button id="botonAñadir" type="button" class="btn btn-warning">
                     <i class=" fa-solid fa-file-circle-plus"></i> Agregar producto
                 </button>
             </div>
@@ -402,7 +402,7 @@ a {
         <!-- Bootstrap-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-        </script>
+            </script>
         <!-- jQuery -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <!-- DataTable -->
